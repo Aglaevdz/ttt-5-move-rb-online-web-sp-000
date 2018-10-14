86673033625c1fb5dbe2501929c658be75556d25
Aglae Verduzco <aglae.cv@gmail.com>
2018-10-14 23:35:49 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 def input_to_index(index)
-  index = 'index.to_i - 1'
+  index = index.to_i - 1
 end
 def move(board, index, value = "X")
   puts board[index.to_i] = value
